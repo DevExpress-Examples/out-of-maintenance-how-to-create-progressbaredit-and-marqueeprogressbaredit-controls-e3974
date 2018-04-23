09235c3b6fa70d71cdf7ecf6ccb2ac315325075a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create ProgressBarEdit and MarqueeProgressBarEdit controls
+
+
+<p>This example shows how to create ProgressBarEdit and MarqueeProgressBarEdit controls.</p>
+
+<br/>
+
+
---
+++ VB/DXSample_ProgressBar/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace DXSample_ProgressBar
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
